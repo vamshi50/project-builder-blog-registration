cf36cce888325438a19f2349993de82ce835a240
vamshi50 <telukuntlavamshi50@gmail.com>
2020-08-10 22:30:27 +0530

    Done

--- src/businesslogic/RegistrationValidation.java
+++ src/businesslogic/RegistrationValidation.java
@@ -1,2 +1,58 @@
 package businesslogic;
+import java.util.regex.Matcher; 
+import java.util.regex.Pattern;
 
+ public class  RegistrationValidation 
+{
+	public boolean checkUserDetails(String email, String password, String confirmPassword)
+	{
+		boolean a=validPassword(password, confirmPassword);
+		boolean b=validEmail(email);
+		if(a && b)
+			return true;
+		else
+			return false;
+	}
+	private boolean validPassword(String password, String confirmPassword)
+	{
+		if(password.length()==0)
+			return false;
+		if(!password.equals(confirmPassword))
+			return false;
+			if(!((password.length()>=8) && (password.length()<=20)))
+				return false;
+			
+			char a[]=password.toCharArray();
+			int capital=0;
+			int lower=0;
+			int num=0;
+			int special=0;
+			for(int i=0; i<password.length(); i++)
+			{
+				if((a[i]>=65) &&(a[i]<=90))
+						capital ++;
+					else if ((a[i]>=97) && (a[i]<=122))
+					lower ++;
+						else if ((a[i]>=48) && (a[i]<=58))
+						num ++;
+						else
+						special++;
+				}
+					if((capital>0)&&(lower>0)&&(num>0))
+							return true;
+						else
+							return false;
+	}
+	private boolean validEmail(String email)
+	{
+		String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+		Pattern pat = Pattern.compile(emailRegex);
+        if(pat.matcher(email).matches())
+            return true;
+						return false;
+	}
+
+}
--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -11,6 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import businesslogic.RegistrationValidation;
+import model.User;
+
 
 
 @WebServlet(urlPatterns= {"/signup"})
@@ -31,12 +34,12 @@ public class SignUpController extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	/*	String email = request.getParameter("email"); 
+    	String email = request.getParameter("email"); 
 		String password = request.getParameter("password"); 
 		String confirmPassword = request.getParameter("confirmPassword"); 
 		LocalDateTime date= LocalDateTime.now(); 
-		
-
+		 RegistrationValidation checkUser = new  RegistrationValidation() ;
+         checkUser.checkUserDetails(email,password,confirmPassword);
 		
 		if(checkUser.checkUserDetails(email, password, confirmPassword))
 		{
@@ -58,7 +61,6 @@ public class SignUpController extends HttpServlet {
 			rd.forward(request, response);
 		}
 		
-		*/
 	}
 
 }
